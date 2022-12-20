206990c3263ffbda33c5fbc867da2df353ab348e
Dmiraculous <106762511+77679@users.noreply.github.com>
2022-12-19 22:47:44 -0800

    Update 100-let_me_const.js

--- 0x12-javascript-warm_up/100-let_me_const.js
+++ 0x12-javascript-warm_up/100-let_me_const.js
@@ -1,12 +1,2 @@
 #!/usr/bin/node
-// computes and prints a factorial
-
-function factorial (n) {
-  if ((isNaN(n)) || (n === 1)) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
-  }
-}
-
-console.log(factorial(parseInt(process.argv[2])));
+myVar = 333;
