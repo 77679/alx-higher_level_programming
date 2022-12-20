f527b373e2dd58e7d6ce9efb13c7afab50c2d61d
Dmiraculous <106762511+77679@users.noreply.github.com>
2022-12-19 22:48:22 -0800

    Update 101-call_me_moby.js

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-// executes x times a function.
-
-exports.callMeMoby = function (x, theFunction) {
+exports.callMeMoby = function (x, callback) {
   for (let i = 0; i < x; i++) {
-    theFunction();
+    callback();
   }
 };
