32243dd0026c46e5c95386229957c7c7677c01b3
77679 <106762511+77679@users.noreply.github.com>
2022-12-21 09:35:50 -0800

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
-function factorial (n) {
-  if (isNaN(n) || n < 2) return 1;
-  else return n * factorial(n - 1);
+
+function factorial (num) {
+  if (num === 0) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+if (isNaN(process.argv[2])) {
+  process.argv[2] = 1;
 }
 console.log(factorial(process.argv[2]));
