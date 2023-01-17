4e1f7a149b45129aa3f3fa8fbaf58f82e64ef94f
77679 <106762511+77679@users.noreply.github.com>
2023-01-17 12:03:53 -0800

    Create 4-hbtn_status.py

---
+++ 0-hbtn_status.py/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+Script that fetches https://intranet.hbtn.io/status
+'''
+import requests
+
+if __name__ == "__main__":
+    url = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(url.text)))
+    print('\t- content: {}'.format(url.text))
