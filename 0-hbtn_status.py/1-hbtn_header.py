7c0af4964143a80de70c9aca793037cf886f333f
77679 <106762511+77679@users.noreply.github.com>
2023-01-17 12:12:04 -0800

    Update 1-hbtn_header.py

--- 0-hbtn_status.py/1-hbtn_header.py
+++ 0-hbtn_status.py/1-hbtn_header.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-'''
-Script that fetches https://intranet.hbtn.io/status
-'''
-from urllib import request
+"""Displays the X-Request-Id header variable of a request to a given URL.
+Usage: ./1-hbtn_header.py <URL>
+"""
 import sys
+import urllib.request
 
-if __name__ == '__main__':
-    with request.urlopen(sys.argv[1]) as response:
-        print(response.getheader('X-Request-Id'))
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
